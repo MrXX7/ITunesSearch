03b85571b70ce58d4b4e0cfed804d0da4c1f255f
Oncu <oohancan@gmail.com>
2022-08-18 20:12:29 +0300

    add class AlbumListViewModel func fetchAlbums for AlbumListViewModel

--- ITunesSearch/ViewModel/AlbumListViewModel.swift
+++ ITunesSearch/ViewModel/AlbumListViewModel.swift
@@ -11,7 +11,26 @@ import Foundation
 // https://itunes.apple.com/search?term=jack+johnson&entity=song&limit=5
 // https://itunes.apple.com/search?term=jack+johnson&entity=movie&limit=5
 
-class AlbumListViewModel {
+class AlbumListViewModel: ObservableObject {
     
+    @Published var searchTerm: String = "Jack Johnson"
+    @Published var albums: [Album] = [Album]()
     
-}
+    func fetchAlbums(for searchTerm: String) {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson&entity=album&limit=5") else {
+            return
+        }
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            if let error = error {
+                print("error: \(error.localizedDescription)")
+            } else if let data = data {
+                do{
+             let result = try JSONDecoder().decode(AlbumResult.self, from: data)
+                    self.albums = result.results
+                } catch {
+                    print("decoding error: \(error.localizedDescription)")
+                }
+            }
+        })
+                                   }
+                                   }
