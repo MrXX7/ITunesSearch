a1d5a37f470e690631835e86a4947e909689435c
Oncu <oohancan@gmail.com>
2022-08-18 17:31:40 +0300

    Initial Commit

---
+++ ITunesSearch/ITunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITunesSearchApp.swift
+//  ITunesSearch
+//
+//  Created by Oncu Can on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ITunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
