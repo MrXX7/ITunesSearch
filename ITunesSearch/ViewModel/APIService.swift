948e4848c3504cddf04d22cb8e5c0a1cb99e97ee
Oncu <oohancan@gmail.com>
2022-08-19 16:28:20 +0300

    add class APIService

--- ITunesSearch/ViewModel/APIService.swift
+++ ITunesSearch/ViewModel/APIService.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+enum EntityType: String {
+    case album
+    case song
+    case movie
+}
+
+class APIService {
+    
+    func fetchAlbums(searchTerm: String, page: Int, limit: Int, completion: @escaping(Result<AlbumResult, APIError>) -> Void) {
+        let url = createURL(for: searchTerm, page: page, limit: limit)
+        fetch(type: AlbumResult.self, url: url, completion: completion)
+    }
+    
+    func fetch<T: Decodable>(type: T.Type, url: URL?, completion: @escaping (Result<T, APIError>) -> Void) {
+        
+        guard let url = url else {
+            let error = APIError.badURL
+            completion(Result.failure(error))
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error = error as? URLError {
+                completion(Result.failure(APIError.urlSession(error)))
+            } else if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
+                completion(Result.failure(APIError.badResponse(response.statusCode)))
+        } else if let data = data {
+            
+            do {
+                let result = try JSONDecoder().decode(type, from: data)
+                completion(Result.success(result))
+            } catch {
+                completion(Result.failure(.decoding(error as? DecodingError)))
+            }
+        }
+    }.resume()
+    
+}
+    func createURL(for searchTerm: String, type: EntityType = .album, page: Int, limit: Int) -> URL? {
+        //    https://itunes.apple.com/search?term=jack+johnson&entity=album&limit=5&offset=10
+        let baseURL = "https://itunes.apple.com/search"
+        
+        let offset = page * limit
+        
+        let queryItems = [URLQueryItem(name: "term", value: "searchTerm"),
+                          URLQueryItem(name: "entity", value: type.rawValue),
+                          URLQueryItem(name: "limit", value: String(limit)),
+                          URLQueryItem(name: "offset", value: String(offset))]
+        
+        var components = URLComponents(string: baseURL)
+        components?.queryItems = queryItems
+        return components?.url
+    }
+}
--- ITunesSearch/ViewModel/AlbumListViewModel.swift
+++ ITunesSearch/ViewModel/AlbumListViewModel.swift
@@ -21,11 +21,7 @@ class AlbumListViewModel: ObservableObject {
         case error(String)
     }
     
-    enum EntityType: String {
-        case album
-        case song
-        case movie
-    }
+    
     
     @Published var searchTerm: String = ""
     @Published var albums: [Album] = [Album]()
@@ -38,6 +34,8 @@ class AlbumListViewModel: ObservableObject {
     
     let limit: Int = 20
     var page: Int = 0
+    
+    let service = APIService()
     
     var subscriptions = Set<AnyCancellable>()
     
@@ -66,100 +64,22 @@ class AlbumListViewModel: ObservableObject {
             return
         }
         state = .isLoading
-        let url = createURL(for: searchTerm)
         
-        fetch(type: AlbumResult.self, url: url) { [weak self] result in
+        service.fetchAlbums(searchTerm: searchTerm, page: page, limit: limit) { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let results):
-                        for album in results.results {
-                            self?.albums.append(album)
-                        }
-                        self?.page += 1
-                        self?.state = (results.results.count == self?.limit) ? .good : .loadedAll
-                        print("fetched \(results.resultCount)")
+                    for album in results.results {
+                        self?.albums.append(album)
+                    }
+                    self?.page += 1
+                    self?.state = (results.results.count == self?.limit) ? .good : .loadedAll
+                    print("fetched \(results.resultCount)")
                     
                 case .failure(let error):
                     self?.state = .error("could not load: \(error.localizedDescription)")
                 }
             }
         }
-        
-        guard let url = createURL(for: searchTerm) else {
-            return
-        }
-        print(url.absoluteString)
-        print("start fetching data for \(searchTerm)")
-        
-        
-        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            
-            if let error = error {
-                print("urlsession error: \(error.localizedDescription)")
-                DispatchQueue.main.async {
-                    self?.state = .error("could not load: \(error.localizedDescription)")
-                }
-            } else if let data = data {
-                
-                do {
-             let result = try JSONDecoder().decode(AlbumResult.self, from: data)
-                    DispatchQueue.main.async {
-                        for album in result.results {
-                            self?.albums.append(album)
-                        }
-                        self?.page += 1
-                        self?.state = (result.results.count == self?.limit) ? .good : .loadedAll
-                        print("fetched \(result.resultCount)")
-                    }
-                } catch {
-                    print("decoding error: \(error)")
-                    DispatchQueue.main.async {
-                        self?.state = .error("could not get data: \(error.localizedDescription)")
-                    }
-                }
-            }
-            
-        }.resume()
-   }
-    func fetch<T: Decodable>(type: T.Type, url: URL?, completion: @escaping (Result<T, APIError>) -> Void) {
-        
-        guard let url = url else {
-            let error = APIError.badURL
-            completion(Result.failure(error))
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            if let error = error as? URLError {
-                completion(Result.failure(APIError.urlSession(error)))
-            } else if let response = response as? HTTPURLResponse, !(200...299).contains(response.statusCode) {
-                completion(Result.failure(APIError.badResponse(response.statusCode)))
-        } else if let data = data {
-            
-            do {
-                let result = try JSONDecoder().decode(type, from: data)
-                completion(Result.success(result))
-            } catch {
-                completion(Result.failure(.decoding(error as? DecodingError)))
-            }
-        }
-    }.resume()
-    
-}
-    
-    func createURL(for searchTerm: String, type: EntityType = .album) -> URL? {
-        //    https://itunes.apple.com/search?term=jack+johnson&entity=album&limit=5&offset=10
-        let baseURL = "https://itunes.apple.com/search"
-        
-        let offset = page * limit
-        
-        let queryItems = [URLQueryItem(name: "term", value: "searchTerm"),
-                          URLQueryItem(name: "entity", value: type.rawValue),
-                          URLQueryItem(name: "limit", value: String(limit)),
-                          URLQueryItem(name: "offset", value: String(offset))]
-        
-        var components = URLComponents(string: baseURL)
-        components?.queryItems = queryItems
-        return components?.url
     }
 }
