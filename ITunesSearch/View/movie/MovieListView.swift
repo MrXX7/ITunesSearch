0d7f4accf219e6be48eacdebd23317d1f9c951d7
Oncu <oohancan@gmail.com>
2022-08-19 17:55:05 +0300

    add MovieListView

---
+++ ITunesSearch/View/movie/MovieListView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListView.swift
+//  ITunesSearch
+//
+//  Created by Oncu Can on 19.08.2022.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
--- ITunesSearch/ViewModel/MovieListViewModel.swift
+++ ITunesSearch/ViewModel/MovieListViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+class MovieListViewModel: ObservableObject {
+    
+    @Published var searchTerm: String = ""
+    @Published var movies: [Movie] = [Movie]()
+    @Published var state: FetchState = .good
+    
+    private let service = APIService()
+
+    
+        
+        let limit: Int = 20
+        var page: Int = 0
+   
+    func fetchMovies(for searchTerm: String) {
+        
+        guard !searchTerm.isEmpty else {
+            return
+        }
+        
+        guard state == FetchState.good else {
+            return
+        }
+        state = .isLoading
+        
+        service.fetchMovies(searchTerm: searchTerm, page: page, limit: limit) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let results):
+                    for movie in results.results {
+                        self?.movies.append(movie)
+                    }
+                    self?.page += 1
+                    self?.state = (results.results.count == self?.limit) ? .good : .loadedAll
+                    print("fetched \(results.resultCount)")
+                    
+                case .failure(let error):
+                    self?.state = .error("could not load: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+}
